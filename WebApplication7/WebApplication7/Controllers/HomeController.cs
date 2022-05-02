930ec1bb0b9d7bf7053da62cbaa6d872f163f23f
Jigar <jigarvaria08@gmail.com>
2022-05-02 12:22:31 +0530

    code commit for projrct init commit

---
+++ WebApplication7/WebApplication7/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication7.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["Message1"] = "view data";
+            ViewBag.Message2 = "view bag";
+            TempData["Message3"] = "India";
+            string[] Rivers = { "Ganga","jamna","kaveri","Godavari"};
+
+            TempData["river"] = Rivers;
+            return View();
+        }
+
+        public ActionResult About()
+        {  
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
